1c0b205c1bac6ade8fb4053ec2d676b4c1d64fda
Alexander Kastil <alexander.kastil@integrations.at>
2025-06-05 10:39:16 +0200

    net 9 upgrade

--- infra/cli/create-api.sh
+++ infra/cli/create-api.sh
@@ -7,4 +7,4 @@ az group create -n $grp -l $loc
 
 az appservice plan create -n $appPlan -g $grp --sku FREE 
 
-az webapp create -n $app -g $grp --plan $appPlan --runtime "DOTNET|6.0"
+az webapp create -n $app -g $grp --plan $appPlan --runtime "DOTNET|9.0"
