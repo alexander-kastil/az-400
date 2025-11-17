0790c67353a9c3347517581f749e93c755182eb5
Alexander Kastil <alexander.kastil@integrations.at>
2025-11-17 11:01:37 +0100

    Refactor AppConfig and Program classes for nullable reference types
    - Updated AppSettings properties to use nullable types for better null safety.
    - Modified Logging and LogLevel classes to allow nullable LogLevel property.
    - Adjusted AppConfig class to use nullable types for Logging, AllowedHosts, Settings, and FeatureManagement.
    - Enhanced Program.cs to ensure safe access to KeyVault and AppConfigEndpoint properties using null-forgiving operator.
    - Improved null handling in endpoint methods for configuration retrieval.

--- src/services/config-service/Program.cs
+++ src/services/config-service/Program.cs
@@ -9,8 +9,8 @@ IConfiguration Configuration = builder.Configuration;
 builder.Services.AddSingleton<IConfiguration>(Configuration);
 
 // Add Key Vault Client to DI
-var cfg = Configuration.Get<AppConfig>();
-var client = new SecretClient(new Uri($"https://{cfg.Settings.KeyVault}"), new DefaultAzureCredential());
+var cfg = Configuration.Get<AppConfig>()!;
+var client = new SecretClient(new Uri($"https://{cfg.Settings!.KeyVault!}"), new DefaultAzureCredential());
 builder.Services.AddSingleton<SecretClient>(client);
 
 builder.Services.AddEndpointsApiExplorer();
@@ -27,29 +27,29 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapGet("/getConfig", ( [FromServices]IConfiguration config) =>
+app.MapGet("/getConfig", ([FromServices] IConfiguration config) =>
 {
-    var cfg = config.Get<AppConfig>();
+    var cfg = config.Get<AppConfig>()!;
     return cfg;
 })
 .WithName("GetConfig")
 .WithOpenApi();
 
 
-app.MapGet("/getSecretFromVault", async ( [FromServices]IConfiguration config, [FromServices]SecretClient secretClient) =>
+app.MapGet("/getSecretFromVault", async ([FromServices] IConfiguration config, [FromServices] SecretClient secretClient) =>
 {
-    var response =  await secretClient.GetSecretAsync("conSQLite");     
-    return response.Value.Value;
+    var response = await secretClient.GetSecretAsync("conSQLite");
+    return response.Value.Value!;
 })
 .WithName("GetSecretFromVault")
 .WithOpenApi();
 
-app.MapGet("/getFromAppConfig/{ConfigItem}", ( [FromServices]IConfiguration config, string ConfigItem) =>
+app.MapGet("/getFromAppConfig/{ConfigItem}", ([FromServices] IConfiguration config, string ConfigItem) =>
 {
-    var cfg = config.Get<AppConfig>();
-    var client = new ConfigurationClient(new Uri(cfg.Settings.AppConfigEndpoint), new DefaultAzureCredential());
+    var cfg = config.Get<AppConfig>()!;
+    var client = new ConfigurationClient(new Uri(cfg.Settings!.AppConfigEndpoint!), new DefaultAzureCredential());
     ConfigurationSetting setting = client.GetConfigurationSetting(ConfigItem);
-    return setting.Value;
+    return setting.Value!;
 })
 .WithName("GetFromAppConfig")
 .WithOpenApi();
