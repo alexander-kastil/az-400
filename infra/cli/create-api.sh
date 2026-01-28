cf4d4be9a8f8db8012ae2f9bae5dc43487852851
Alexander Kastil <alexander.kastil@integrations.at>
2026-01-28 16:20:24 +0100

    refactor: update create-api.sh and add catalog-ci-unittest-template.yml
    - Modified create-api.sh to use dynamic environment and application names.
    - Changed app service plan name to reflect the environment variable.
    - Updated .NET runtime version from 9.0 to 10.0 in create-api.sh.
    - Added new catalog-ci-unittest-template.yml for CI unit testing configuration.

--- infra/cli/create-api.sh
+++ infra/cli/create-api.sh
@@ -1,10 +1,11 @@
+env=$1
+app=$2
+appPlan=foodplan-$env
 loc=westeurope
-grp=az400-m06-IaC
-appPlan=foodplan-iac
-app=$1
+grp=az400-$env
 
 az group create -n $grp -l $loc
 
 az appservice plan create -n $appPlan -g $grp --sku FREE 
 
-az webapp create -n $app -g $grp --plan $appPlan --runtime "DOTNET|9.0"
+az webapp create -n $app -g $grp --plan $appPlan --runtime "DOTNET|10.0"
